c9c3ba5b51cecb5b131182e10d6435461e4093c3
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 11:36:42 +0200

    feat: create a while loop that runs while unvisited is not empty. Use the pass keyword to fill the loop body

--- lecture.py
+++ lecture.py
@@ -413,5 +413,20 @@ def shortest_path(graph, start):
     
 #shortest_path(my_graph, 'A')
 
+# Step 34
+
+# Before the print call, create a while loop that runs while unvisited is not empty. 
+# Use the pass keyword to fill the loop body.
+
+def shortest_path(graph, start):
+    unvisited = list(graph)
+    distances = {node: 0 if node == start else float('inf') for node in graph}
+    paths = {node: [] for node in graph}
+    paths[start].append(start)
+    while unvisited:
+        pass
+    print(f'Unvisited: {unvisited}\nDistances: {distances}\nPaths: {paths}')
+    
+#shortest_path(my_graph, 'A')
 
 
