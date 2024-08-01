894f0a5b8f2ba2a087e4dc702c0c0dd462db5e98
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 11:32:18 +0200

    feat: add a print() call and pass in the following string to see the values of the variables you have created: f'Unvisited: {unvisited}\nDistances: {distances}'

--- lecture.py
+++ lecture.py
@@ -271,6 +271,20 @@ def shortest_path(graph, start):
         else:
             distances[node] = float('inf')
 
+# Step 25
 
+# After your for loop, add a print() call and pass in the following string to see the values of 
+# the variables you have created: f'Unvisited: {unvisited}\nDistances: {distances}'.
+
+def shortest_path(graph, start):
+    unvisited = []
+    distances = {}
+    for node in graph:
+        unvisited.append(node)
+        if node == start:
+            distances[node] = 0
+        else:
+            distances[node] = float('inf')
+    print(f'Unvisited: {unvisited}\nDistances: {distances}')
 
 
