ede1391d2f485f20e0c1504bd9f6c9d6a052313c
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 11:31:31 +0200

    feat: Create an else clause and assign an infinite value to the node in the distances dictionary. For that, use the float() function with the string inf as argument to generate a floating point number representing the positive infinity

--- lecture.py
+++ lecture.py
@@ -252,6 +252,24 @@ def shortest_path(graph, start):
         if node == start:
             distances[node] = 0
 
+# Step 24
+
+# At the beginning, all the other nodes in the graph are considered to be at infinite distance from the 
+# source node, because the distance has not been determined yet.
+
+#Create an else clause and assign an infinite value to the node in the distances dictionary. 
+# For that, use the float() function with the string inf as argument to generate a floating point number 
+# representing the positive infinity.
+
+def shortest_path(graph, start):
+    unvisited = []
+    distances = {}
+    for node in graph:
+        unvisited.append(node)
+        if node == start:
+            distances[node] = 0
+        else:
+            distances[node] = float('inf')
 
 
 
