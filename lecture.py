808b673d4ce1457dd6a26faa35f9f6b9a6de7f6d
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 11:35:22 +0200

    feat: Use the append() method to append start to the paths[start] list

--- lecture.py
+++ lecture.py
@@ -370,4 +370,20 @@ def shortest_path(graph, start):
     
 shortest_path(my_graph, 'A')
 
+# Step 31
+
+# Since the algorithm begins its assessment from the starting node, after creating the paths dictionary, 
+# you need to add the starting node to its own list in the paths dictionary.
+
+# Use the append() method to append start to the paths[start] list.
+
+def shortest_path(graph, start):
+    unvisited = list(graph)
+    distances = {node: 0 if node == start else float('inf') for node in graph}
+    paths = {node: [] for node in graph}
+    paths[start].append(start)
+    print(f'Unvisited: {unvisited}\nDistances: {distances}')
+    
+shortest_path(my_graph, 'A')
+
 
