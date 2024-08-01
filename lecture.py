ac241f1c6d41b40562455d13a86affbf327ac656
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 11:48:05 +0200

    feat: insert exemple to test function

--- lecture.py
+++ lecture.py
@@ -646,4 +646,22 @@ for node in targets_to_print:
 # return distances, paths
 shortest_path(my_graph, 'A')
 
+# Step 53
 
+# Now, you are going to test your function with another graph. 
+# Change my_graph into the following graph:
+
+{
+    'A': [('B', 5), ('C', 3), ('E', 11)],
+    'B': [('A', 5), ('C', 1), ('F', 2)],
+    'C': [('A', 3), ('B', 1), ('D', 1), ('E', 5)],
+    'D': [('C', 1), ('E', 9), ('F', 3)],
+    'E': [('A', 11), ('C', 5), ('D', 9)],
+    'F': [('B', 2), ('D', 3)]
+}
+
+# Step 54
+
+# As a final step, modify your function call passing F as the third argument and check the output.
+
+# With that, the shortest path algorithm is complete.
